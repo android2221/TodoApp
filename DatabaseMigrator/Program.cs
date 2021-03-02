12698d0e48ddfbfb3712b5ad0a9960a56279920b
Andy <andy@andys-mbp.lan>
2021-03-02 08:09:58 -0700

    added database and migrations from old project

---
+++ DatabaseMigrator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using DbUp;
+
+namespace DatabaseMigrator
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString = "Server=database;Database=TodoDB;User Id=SA;Password=p@ssw0rd1;";
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
---
+++ DatabaseMigrator/Scripts/0001.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Todos(
+    ID int IDENTITY(1,1) PRIMARY KEY,
+    Title varchar(255),
+    Note varchar(max)
+);
